08b022de9d73b6967b1aa6e99acd2331c37edabe
Thiago Degobi <sr.degobi@gmail.com>
2025-01-08 22:11:41 -0300

    login finalziado

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -7,14 +7,7 @@ import { MessageService } from 'primeng/api';
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styles: [`
-        :host ::ng-deep .pi-eye,
-        :host ::ng-deep .pi-eye-slash {
-            transform:scale(1.6);
-            margin-right: 1rem;
-            color: var(--primary-color) !important;
-        }
-    `]
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
 
